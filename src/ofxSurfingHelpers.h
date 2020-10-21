7d08006994687eda27aa92b6806d97f1960a998b
moebiussurfing <moebiussurfing@users.noreply.github.com>
2020-10-21 07:27:02 -0300

    Update ofxSurfingHelpers.h

--- src/ofxSurfingHelpers.h
+++ src/ofxSurfingHelpers.h
@@ -103,7 +103,7 @@ namespace ofxSurfingHelpers {
 	{
 		ofLogVerbose(__FUNCTION__) << g.getName() << " to " << path;
 		ofLogVerbose(__FUNCTION__) << "parameters: \n" << g.toString();
-		
+
 		//CheckFolder(path);
 
 		ofXml settings;
@@ -157,7 +157,7 @@ namespace ofxSurfingHelpers {
 			ofLogVerbose(__FUNCTION__) << g.getName() << " to " << path;
 			ofLogVerbose(__FUNCTION__) << "parameters: \n" << g.toString();
 		}
-		
+
 		//ofxSurfingHelpers::CheckFolder(path);
 
 		ofJson settings;
@@ -299,7 +299,7 @@ namespace ofxSurfingHelpers {
 			// text shadow
 			if (useShadow) {
 				ofSetColor(colorShadow);
-				font.drawString(text, x+1, y+1);
+				font.drawString(text, x + 1, y + 1);
 			}
 
 			// text
@@ -317,6 +317,51 @@ namespace ofxSurfingHelpers {
 	}
 	inline float getHeightBBtextBoxed(ofTrueTypeFont &font, string text) {
 		return (font.getStringBoundingBox(text, 0, 0)).getHeight();
+	}
+
+	//---
+
+	// original code copied from: ofxFilikaUtils.h
+#define SECS_PER_MIN 60
+#define SECS_PER_HOUR 3600
+	//--------------------------------------------------------------
+	inline std::string calculateTime(float _time) {
+
+		int seconds;
+		int minutes;
+		int mins_left;
+		int secs_left;
+
+		seconds = (/*gameTimeSeconds - */int(_time));
+		minutes = (/*gameTimeSeconds - */int(_time)) / SECS_PER_MIN;
+		mins_left = minutes % SECS_PER_MIN;
+		secs_left = seconds % SECS_PER_MIN;
+
+		std::string mins;
+		std::string secs;
+
+		if (mins_left < 10) {
+			mins = "0" + ofToString(mins_left);
+		}
+		else {
+			mins = ofToString(mins_left);
+		}
+
+		if (secs_left < 10) {
+			secs = "0" + ofToString(secs_left);
+		}
+		else {
+			secs = ofToString(secs_left);
+		}
+
+		//cout << ofGetElapsedTimeMillis() / 1000 << endl;
+		//cout << "remaining time : " << mins_left << " : " <<  secs_left << endl;
+		//cout << "remaining time : " << mins << " : " <<  secs << endl;
+
+		if (mins_left < 0 || secs_left < 0)
+			return "00:00";
+		else
+			return (mins + ":" + secs);
 	}
 
 };// ofxSurfingHelpers
