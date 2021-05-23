0ccd70b2d9d3a2ad975793e6b829d5d22ebfa682
moebius surfing <moebiussurfing@gmail.com>
2021-05-23 03:40:08 -0300

    +

--- src/ofxSurfing_ImGui_LayoutManager.cpp
+++ src/ofxSurfing_ImGui_LayoutManager.cpp
@@ -9,70 +9,15 @@ ofxSurfing_ImGui_LayoutManager::~ofxSurfing_ImGui_LayoutManager() {
 };
 
 //--------------------------------------------------------------
-void ofxSurfing_ImGui_LayoutManager::setup(ofxImGui::Gui & _gui) {
+void ofxSurfing_ImGui_LayoutManager::setup() {//using internal instantiated gui
+	setup_ImGui();
+}
+
+//--------------------------------------------------------------
+void ofxSurfing_ImGui_LayoutManager::setup(ofxImGui::Gui & _gui) {//using external instantiated gui
 	guiPtr = &_gui;
 
 	setup_ImGui();
-}
-
-//--------------------------------------------------------------
-void ofxSurfing_ImGui_LayoutManager::setup() {
-	setup_ImGui();
-}
-
-//--------------------------------------------------------------
-void ofxSurfing_ImGui_LayoutManager::begin() {
-	if (guiPtr != nullptr) guiPtr->begin();
-	else gui.begin();
-
-	//panels sizes
-	float xx = 10;
-	float yy = 10;
-	float ww = PANEL_WIDGETS_WIDTH;
-	float hh = PANEL_WIDGETS_HEIGHT;
-
-	//widgets sizes
-	float _spcx;
-	float _spcy;
-	float _w100;
-	float _h100;
-	float _w99;
-	float _w50;
-	float _w33;
-	float _w25;
-	float _h;
-
-	ImGuiWindowFlags flagsw = auto_resize ? ImGuiWindowFlags_AlwaysAutoResize : ImGuiWindowFlags_None;
-
-	if (customFont != nullptr) ImGui::PushFont(customFont);
-	ImGui::PushStyleVar(ImGuiStyleVar_WindowMinSize, ImVec2(ww, hh));
-}
-
-//--------------------------------------------------------------
-void ofxSurfing_ImGui_LayoutManager::end() {
-
-	bool bUseAdvancedSubPanel = true;
-	if (bUseAdvancedSubPanel) {
-		ImGui::Dummy(ImVec2(0.0f, 2.0f));
-		if (ImGui::CollapsingHeader("ADVANCED"))
-		{
-			ofxImGui::AddParameter(auto_resize);
-			ofxImGui::AddParameter(bLockMouseByImGui);
-			//ofxImGui::AddParameter(auto_lockToBorder);
-		}
-	}
-
-	ImGui::PopStyleVar();
-	if (customFont != nullptr) ImGui::PopFont();
-
-	//mouse lockers
-	bLockMouseByImGui = false;
-	bLockMouseByImGui = bLockMouseByImGui | ImGui::IsWindowHovered(ImGuiHoveredFlags_AnyWindow);
-	bLockMouseByImGui = bLockMouseByImGui | ImGui::IsWindowHovered(ImGuiHoveredFlags_RootAndChildWindows);
-	bLockMouseByImGui = bLockMouseByImGui | ImGui::IsWindowHovered(ImGuiHoveredFlags_AllowWhenBlockedByActiveItem);
-
-	if (guiPtr != nullptr) guiPtr->end();
-	else gui.end();
 }
 
 //--------------------------------------------------------------
@@ -97,8 +42,6 @@ void ofxSurfing_ImGui_LayoutManager::setup_ImGui()
 	fontName = "telegrama_render.otf"; //  WARNING: will crash if font not present!
 	fontSizeParam = 11;
 
-	//-
-
 	std::string _path = "assets/fonts/"; // assets folder
 	ofFile fileToRead(_path); // a file that exists
 	bool b = fileToRead.exists();
@@ -109,7 +52,34 @@ void ofxSurfing_ImGui_LayoutManager::setup_ImGui()
 
 	if (customFont != nullptr) io.FontDefault = customFont;
 
+	//-
+
 	// theme
 	ofxSurfingHelpers::ImGui_ThemeMoebiusSurfing();
 	//ofxSurfingHelpers::ImGui_ThemeModernDark();
+}
+
+//--------------------------------------------------------------
+void ofxSurfing_ImGui_LayoutManager::begin() {
+	if (guiPtr != nullptr) guiPtr->begin();
+	else gui.begin();
+
+	if (customFont != nullptr) ImGui::PushFont(customFont);
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowMinSize, ImVec2(ww, hh));//minimal size
+}
+
+//--------------------------------------------------------------
+void ofxSurfing_ImGui_LayoutManager::end() {
+
+	ImGui::PopStyleVar();//minimal size
+	if (customFont != nullptr) ImGui::PopFont();
+
+	//mouse lockers
+	bLockMouseByImGui = false;
+	bLockMouseByImGui = bLockMouseByImGui | ImGui::IsWindowHovered(ImGuiHoveredFlags_AnyWindow);
+	bLockMouseByImGui = bLockMouseByImGui | ImGui::IsWindowHovered(ImGuiHoveredFlags_RootAndChildWindows);
+	bLockMouseByImGui = bLockMouseByImGui | ImGui::IsWindowHovered(ImGuiHoveredFlags_AllowWhenBlockedByActiveItem);
+
+	if (guiPtr != nullptr) guiPtr->end();
+	else gui.end();
 }
--- src/ofxSurfing_ImGui_LayoutManager.h
+++ src/ofxSurfing_ImGui_LayoutManager.h
@@ -2,11 +2,7 @@
 
 #include "ofMain.h"
 
-#include "ofxImGui.h"
-#include "imgui_internal.h"
 #include "ofxSurfing_ImGui.h"
-#include "ofxSurfing_ImGui_Themes.h"
-#include "ofxSurfing_ImGui_Widgets.h"
 
 //using namespace ImGui;
 //namespace ofxSurfingHelpers
@@ -14,26 +10,89 @@
 class ofxSurfing_ImGui_LayoutManager
 {
 public:
+
 	ofxSurfing_ImGui_LayoutManager();
 	~ofxSurfing_ImGui_LayoutManager();
 
-public:
-	void setup(ofxImGui::Gui & gui);
 	void setup();
-
-	void setup_ImGui();
-	//void draw();
+	void setup(ofxImGui::Gui & gui);
 	void begin();
 	void end();
+
+private:
+
+	void setup_ImGui();
 
 	ofxImGui::Gui * guiPtr = NULL;
 	ofxImGui::Gui gui;
 
 	ofxImGui::Settings mainSettings = ofxImGui::Settings();
 	ImFont* customFont = nullptr;
+
+public:
 	ofParameter<bool> bGui{ "Show Gui", true };
 	ofParameter<bool> auto_resize{ "Auto Resize", true };//auto resize panel
 	ofParameter<bool> bLockMouseByImGui{ "Mouse Locked", false };//mouse is over gui
-	ofParameter<bool> auto_lockToBorder{ "Lock GUI", false };//force position
+	//ofParameter<bool> auto_lockToBorder{ "Lock GUI", false };//force position
+
+private:
+	bool bUseAdvancedSubPanel = false;//enable advanced sub panel
+
+	//panels sizes
+	float xx = 10;
+	float yy = 10;
+	float ww = PANEL_WIDGETS_WIDTH;
+	float hh = PANEL_WIDGETS_HEIGHT;
+
+	/*
+	//snippet
+	guiManager.begin();
+	{
+		auto mainSettings = ofxImGui::Settings();
+		ImGuiColorEditFlags _flagw = ImGuiWindowFlags_None;
+		string name = "myWindow";
+
+		////widgets sizes
+		//float _spcx;
+		//float _spcy;
+		//float _w100;
+		//float _h100;
+		//float _w99;
+		//float _w50;
+		//float _w33;
+		//float _w25;
+		//float _h;
+
+		if (ofxImGui::BeginWindow(name.c_str(), mainSettings, _flagw))
+		{
+			ofxSurfingHelpers::refreshImGui_WidgetsSizes(_spcx, _spcy, _w100, _h100, _w99, _w50, _w33, _w25, _h);
+
+			//drawWidgets();
+
+			guiManager.drawAdvancedSubPanel();
+		}
+		ofxImGui::EndWindow(mainSettings);
+	}
+	guiManager.end();
+	*/
+
+	//-
+
+public:
+	//--------------------------------------------------------------
+	void drawAdvancedSubPanel() {
+		ImGui::Dummy(ImVec2(0.0f, 2.0f));
+		if (ImGui::CollapsingHeader("ADVANCED"))
+		{
+			ofxImGui::AddParameter(auto_resize);
+			ofxImGui::AddParameter(bLockMouseByImGui);
+			//ofxImGui::AddParameter(auto_lockToBorder);
+		}
+	}
+
+	//--------------------------------------------------------------
+	//void setUseAdvancedSubPanel(bool b) {
+	//	bUseAdvancedSubPanel = b;
+	//}
 };
 //};
