708cb4c18966344421e61d30caf10eff6dc83f52
moebius surfing <moebiussurfing@gmail.com>
2021-10-15 02:25:35 -0300

    added vec ofParams splitter and float normalized converters

--- src/utils/surfingGlmSplit.h
+++ src/utils/surfingGlmSplit.h
@@ -13,6 +13,16 @@
 */
 
 /*
+//params.add(camEulerAngles_Split.x);
+//params.add(camEulerAngles_Split.y);
+//params.add(camEulerAngles_Split.z);
+//params.add(camLookAt_Split.x);
+//params.add(camLookAt_Split.y);
+//params.add(camLookAt_Split.z);
+*/
+
+
+/*
 
 // Example
 // ofApp.h
@@ -35,8 +45,8 @@ public:
 	ofParameter<glm::vec2> v2{ "myVec2", glm::vec2(0.25), glm::vec2(0), glm::vec2(1) };
 	ofParameter<glm::vec3> v3{ "myVec3", glm::vec3(0.25), glm::vec3(0), glm::vec3(1) };
 
-	glmSplitter2 sp_v2;
-	glmSplitter3 sp_v3;
+	surfingGlmSplitter2 sp_v2;
+	surfingGlmSplitter3 sp_v3;
 
 	void setup() {
 		sp_v2.setup(v2);
@@ -61,11 +71,14 @@ public:
 
 */
 
-class glmSplitter4
+//TODO:
+
+/*
+class surfingGlmSplitter4
 {
 public:
-	glmSplitter4() {};
-	~glmSplitter4() {};
+	surfingGlmSplitter4() {};
+	~surfingGlmSplitter4() {};
 
 	//TODO:
 	bool bNormalized = false; // 0 to 1
@@ -88,7 +101,7 @@ public:
 
 	ofParameter<glm::vec4> vecRef{ "-1",glm::vec4(0.5),glm::vec4(0),glm::vec4(1) };
 
-public:
+private:
 	void set(ofParameter<glm::vec4> _v) {
 		x.set(_v.get().x);
 		y.set(_v.get().y);
@@ -106,6 +119,7 @@ public:
 		w.setMax(_v.getMax().w);
 	}
 
+public:
 	void setup(ofParameter<glm::vec4> &_v) {
 		vecRef.makeReferenceTo(_v);
 
@@ -167,21 +181,45 @@ private:
 				}
 				else { return; }
 
-				if (name == x.getName())
-				{
-					vecRef.set(glm::vec4(x, y, z, w));
+				if (!bNormalized) {
+					if (name == x.getName())
+					{
+						vecRef.set(glm::vec4(x, y, z, w));
+					}
+					else if (name == y.getName())
+					{
+						vecRef.set(glm::vec4(x, y, z, w));
+					}
+					else if (name == z.getName())
+					{
+						vecRef.set(glm::vec4(x, y, z, w));
+					}
+					else if (name == w.getName())
+					{
+						vecRef.set(glm::vec4(x, y, z, w));
+					}
 				}
-				else if (name == y.getName())
-				{
-					vecRef.set(glm::vec4(x, y, z, w));
-				}
-				else if (name == z.getName())
-				{
-					vecRef.set(glm::vec4(x, y, z, w));
-				}
-				else if (name == w.getName())
-				{
-					vecRef.set(glm::vec4(x, y, z, w));
+				else if (bNormalized && !bSigned) {
+					if (name == x.getName())
+					{
+						vecRef.set(glm::vec4(
+							ofMap(x, 0, 1, vecRef.getMin().x, vecRef.getMax().x),
+							ofMap(y, 0, 1, vecRef.getMin().y, vecRef.getMax().y),
+							ofMap(z, 0, 1, vecRef.getMin().z, vecRef.getMax().z),
+							ofMap(w, 0, 1, vecRef.getMin().w, vecRef.getMax().w)));
+					}
+					else if (name == y.getName())
+					{
+						vecRef.set(glm::vec4(x, y, z, w));
+					}
+					else if (name == z.getName())
+					{
+						vecRef.set(glm::vec4(x, y, z, w));
+					}
+					else if (name == w.getName())
+					{
+						vecRef.set(glm::vec4(x, y, z, w));
+					}
 				}
 			}
 		});
@@ -189,14 +227,19 @@ private:
 		ofLogNotice() << __FUNCTION__;
 	};
 };
+*/
 
-//-
+//--------------------------------------------------------------
 
-class glmSplitter3
+class surfingGlmSplitter3
 {
 public:
-	glmSplitter3() {};
-	~glmSplitter3() {};
+	surfingGlmSplitter3() {};
+	~surfingGlmSplitter3() {};
+
+	bool bNamesShort = false;
+	bool bNormalized = false; // 0, 1
+	bool bSigned = false; // -1, 1
 
 private:
 	ofEventListener listenerParams;
@@ -214,28 +257,48 @@ public:
 
 	ofParameter<glm::vec3> vecRef{ "-1",glm::vec3(0.5),glm::vec3(0),glm::vec3(1) };
 
-public:
+private:
 	void set(ofParameter<glm::vec3> _v) {
 		x.set(_v.get().x);
 		y.set(_v.get().y);
 		z.set(_v.get().z);
 
 		// limits
-		x.setMin(_v.getMin().x);
-		y.setMin(_v.getMin().y);
-		z.setMin(_v.getMin().z);
-		x.setMax(_v.getMax().x);
-		y.setMax(_v.getMax().y);
-		z.setMax(_v.getMax().z);
+		if (!bNormalized) {
+			x.setMin(_v.getMin().x);
+			y.setMin(_v.getMin().y);
+			z.setMin(_v.getMin().z);
+			x.setMax(_v.getMax().x);
+			y.setMax(_v.getMax().y);
+			z.setMax(_v.getMax().z);
+		}
+		else if (bNormalized && !bSigned) {
+			x.setMin(0.f);
+			y.setMin(0.f);
+			z.setMin(0.f);
+			x.setMax(1.0f);
+			y.setMax(1.0f);
+			z.setMax(1.0f);
+		}
+		else //if (bNormalized && bSigned) 
+		{
+			x.setMin(-1.f);
+			y.setMin(-1.f);
+			z.setMin(-1.f);
+			x.setMax(1.0f);
+			y.setMax(1.0f);
+			z.setMax(1.0f);
+		}
 	}
 
+public:
 	void setup(ofParameter<glm::vec3> &_v) {
 		vecRef.makeReferenceTo(_v);
 
 		std::string n = _v.getName();
-		x.setName(n + " x");
-		y.setName(n + " y");
-		z.setName(n + " z");
+		x.setName(bNamesShort ? "x" : n + " x");
+		y.setName(bNamesShort ? "y" : n + " y");
+		z.setName(bNamesShort ? "z" : n + " z");
 
 		set(_v);
 
@@ -267,16 +330,33 @@ private:
 				return;
 			}
 
+			// Some coord of VEC changed:
 			if (name == vecRef.getName()) {
-				x.setWithoutEventNotifications(vecRef.get().x);
-				y.setWithoutEventNotifications(vecRef.get().y);
-				z.setWithoutEventNotifications(vecRef.get().z);
+				if (!bNormalized)
+				{
+					x.setWithoutEventNotifications(vecRef.get().x);
+					y.setWithoutEventNotifications(vecRef.get().y);
+					z.setWithoutEventNotifications(vecRef.get().z);
+				}
+				else if (bNormalized && !bSigned)
+				{
+					x.setWithoutEventNotifications(ofMap(vecRef.get().x, vecRef.getMin().x, vecRef.getMax().x, 0, 1));
+					y.setWithoutEventNotifications(ofMap(vecRef.get().y, vecRef.getMin().y, vecRef.getMax().y, 0, 1));
+					z.setWithoutEventNotifications(ofMap(vecRef.get().z, vecRef.getMin().z, vecRef.getMax().z, 0, 1));
+				}
+				else //if (bNormalized && bSigned)
+				{
+					x.setWithoutEventNotifications(ofMap(vecRef.get().x, vecRef.getMin().x, vecRef.getMax().x, -1, 1));
+					y.setWithoutEventNotifications(ofMap(vecRef.get().y, vecRef.getMin().y, vecRef.getMax().y, -1, 1));
+					z.setWithoutEventNotifications(ofMap(vecRef.get().z, vecRef.getMin().z, vecRef.getMax().z, -1, 1));
+				}
 
 				x = x;
 				y = y;
 				z = z;
 			}
 
+			// Some splitted coord changed:
 			else {
 				if (p.type() == typeid(ofParameter<float>).name())
 				{
@@ -285,17 +365,68 @@ private:
 				}
 				else { return; }
 
-				if (name == x.getName())
+				if (!bNormalized)
 				{
-					vecRef.set(glm::vec3(x, y, z));
+					if (name == x.getName())
+					{
+						vecRef.set(glm::vec3(x, y, z));
+					}
+					else if (name == y.getName())
+					{
+						vecRef.set(glm::vec3(x, y, z));
+					}
+					else if (name == z.getName())
+					{
+						vecRef.set(glm::vec3(x, y, z));
+					}
 				}
-				else if (name == y.getName())
+				else if (bNormalized && !bSigned)
 				{
-					vecRef.set(glm::vec3(x, y, z));
+					if (name == x.getName())
+					{
+						vecRef.set(glm::vec3(
+							ofMap(x, 0, 1, vecRef.getMin().x, vecRef.getMax().x),
+							ofMap(y, 0, 1, vecRef.getMin().y, vecRef.getMax().y),
+							ofMap(z, 0, 1, vecRef.getMin().z, vecRef.getMax().z)));
+					}
+					else if (name == y.getName())
+					{
+						vecRef.set(glm::vec3(
+							ofMap(x, 0, 1, vecRef.getMin().x, vecRef.getMax().x),
+							ofMap(y, 0, 1, vecRef.getMin().y, vecRef.getMax().y),
+							ofMap(z, 0, 1, vecRef.getMin().z, vecRef.getMax().z)));
+					}
+					else if (name == z.getName())
+					{
+						vecRef.set(glm::vec3(
+							ofMap(x, 0, 1, vecRef.getMin().x, vecRef.getMax().x),
+							ofMap(y, 0, 1, vecRef.getMin().y, vecRef.getMax().y),
+							ofMap(z, 0, 1, vecRef.getMin().z, vecRef.getMax().z)));
+					}
 				}
-				else if (name == z.getName())
+				else //if (bNormalized && bSigned) 
 				{
-					vecRef.set(glm::vec3(x, y, z));
+					if (name == x.getName())
+					{
+						vecRef.set(glm::vec3(
+							ofMap(x, -1, 1, vecRef.getMin().x, vecRef.getMax().x),
+							ofMap(y, -1, 1, vecRef.getMin().y, vecRef.getMax().y),
+							ofMap(z, -1, 1, vecRef.getMin().z, vecRef.getMax().z)));
+					}
+					else if (name == y.getName())
+					{
+						vecRef.set(glm::vec3(
+							ofMap(x, -1, 1, vecRef.getMin().x, vecRef.getMax().x),
+							ofMap(y, -1, 1, vecRef.getMin().y, vecRef.getMax().y),
+							ofMap(z, -1, 1, vecRef.getMin().z, vecRef.getMax().z)));
+					}
+					else if (name == z.getName())
+					{
+						vecRef.set(glm::vec3(
+							ofMap(x, -1, 1, vecRef.getMin().x, vecRef.getMax().x),
+							ofMap(y, -1, 1, vecRef.getMin().y, vecRef.getMax().y),
+							ofMap(z, -1, 1, vecRef.getMin().z, vecRef.getMax().z)));
+					}
 				}
 			}
 		});
@@ -304,15 +435,14 @@ private:
 	};
 };
 
+//--------------------------------------------------------------
 
-//-
-
-
-class glmSplitter2
+/*
+class surfingGlmSplitter2
 {
 public:
-	glmSplitter2() {};
-	~glmSplitter2() {};
+	surfingGlmSplitter2() {};
+	~surfingGlmSplitter2() {};
 
 private:
 	ofEventListener listenerParams;
@@ -329,7 +459,7 @@ public:
 
 	ofParameter<glm::vec2> vecRef{ "-1",glm::vec2(0.5),glm::vec2(0),glm::vec2(1) };
 
-public:
+private:
 	void set(ofParameter<glm::vec2> _v) {
 		x.set(_v.get().x);
 		y.set(_v.get().y);
@@ -341,6 +471,7 @@ public:
 		y.setMax(_v.getMax().y);
 	}
 
+public:
 	void setup(ofParameter<glm::vec2> &_v) {
 		vecRef.makeReferenceTo(_v);
 
@@ -407,3 +538,4 @@ private:
 		ofLogNotice() << __FUNCTION__;
 	};
 };
+*/
