d250ceca4c8cebe1a8c147576a8d7d5aac5607df
moebiusSurfing <moebiussurfing@gmail.com>
2022-10-18 05:34:56 -0300

    Update ofxSurfingConstants.h

--- src/ofxSurfingConstants.h
+++ src/ofxSurfingConstants.h
@@ -4,8 +4,8 @@
 //
 // OPTIONAL // -> defines (modes) are here "to share between addons" in one place
 //
+#define USE_JSON //uncomment to use default xml instead json for ofParameterGroup de/serializers
 //#define USE_IM_GUI
-#define USE_JSON		//uncomment to use default xml instead json for ofParameterGroup de/serializers
 //#define USE_ofxGui
 //
 //---------
@@ -21,6 +21,8 @@
 
 #define FONT_SIZE_BIG 16
 #define FONT_SIZE_SMALL 10
+
+//--
 
 // EXAMPLE TO CHECK IF FONT FILES ARE LOCATED OR NOT TO AVOID CRASHES
 /*
